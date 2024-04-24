b3f1900af6e526a386625bd3b82637c0daf6aedd
Andres Felipe Muñoz Cardenas <143211280+AndresCardenasM@users.noreply.github.com>
2024-04-24 09:50:32 -0500

    Delete screenMatchWithAPI-main/screenMatchWithAPI-main/screenMatch-main/src/com/aluracursos/screenmatch directory

--- screenMatchWithAPI-main/screenMatchWithAPI-main/screenMatch-main/src/com/aluracursos/screenmatch/calculos/Classification.java
+++
@@ -1,6 +0,0 @@
-package com.aluracursos.screenmatch.calculos;
-
-public interface Classification {
-    int getClassification();
-
-}
--- screenMatchWithAPI-main/screenMatchWithAPI-main/screenMatch-main/src/com/aluracursos/screenmatch/models/OMDbTitle.java
+++
@@ -1,5 +0,0 @@
-package com.aluracursos.screenmatch.models;
-
-public record OMDbTitle(String title, String year, String runtime) {
-
-}
