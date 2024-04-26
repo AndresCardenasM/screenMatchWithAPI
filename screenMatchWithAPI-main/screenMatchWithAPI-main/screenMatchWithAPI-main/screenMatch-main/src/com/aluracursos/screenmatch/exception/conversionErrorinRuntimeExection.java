7770e72032ad2b9ea5d1090507155b4bdf93cc85
Andres Felipe Muñoz Cardenas <143211280+AndresCardenasM@users.noreply.github.com>
2024-04-26 07:38:37 -0500

    Delete screenMatchWithAPI-main/screenMatchWithAPI-main/screenMatchWithAPI-main/screenMatch-main/src/com/aluracursos/screenmatch/exception/conversionErrorinRuntimeExection.java

--- screenMatchWithAPI-main/screenMatchWithAPI-main/screenMatchWithAPI-main/screenMatch-main/src/com/aluracursos/screenmatch/exception/conversionErrorinRuntimeExection.java
+++
@@ -1,6 +0,0 @@
-package com.aluracursos.screenmatch.exception;
-
-public class conversionErrorinRuntimeExeption extends Throwable {
-    public conversionErrorinRuntimeExection(String s) {
-    }
-}
