1b18334ce41e24ca0e22b09a9280881b033492da
Andres Felipe Muñoz Cardenas <143211280+AndresCardenasM@users.noreply.github.com>
2024-04-28 14:23:01 -0500

    Delete StarWarsAPI2 directory

--- StarWarsAPI2/src/Movie.java
+++
@@ -1,8 +0,0 @@
-public record Movie(String title,
-                    int episode_id,
-                    String opening_crawl,
-                    String director,
-                    String producer,
-                    String release_date) {
-}
-
